21f74c0431153189c783ce7f0d22a6463c864eaa
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-02 09:39:43 +0200

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ArangeArrayDescending
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()  //We created the array and read some input info from the console
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // numebrs[i]; -> currentNumb
+
+                for (int j = i+1; j < numbers.Length; j++)  ////5 2 4 10 -> arrange this descending // firsNum must be always maxValue;
+                {
+                    if (numbers[i] >= numbers[j])
+                    {
+                        continue;
+                    }
+                    else // if numbers[i] < numbers[j];
+                    {
+                        int tempNub = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = tempNub;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
