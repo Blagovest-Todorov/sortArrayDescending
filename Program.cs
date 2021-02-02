8698366dd118fcadf818bde239ed98ca2bf6c67a
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-02 09:49:12 +0200

    another way to solve the task

--- Program.cs
+++ Program.cs
@@ -16,13 +16,9 @@ namespace ArangeArrayDescending
             {
                 // numebrs[i]; -> currentNumb
 
-                for (int j = i+1; j < numbers.Length; j++)  ////5 2 4 10 -> arrange this descending // firsNum must be always maxValue;
+                for (int j = i + 1; j < numbers.Length; j++)  ////5 2 4 10 -> arrange this descending // firsNum must be always maxValue;
                 {
-                    if (numbers[i] >= numbers[j])
-                    {
-                        continue;
-                    }
-                    else // if numbers[i] < numbers[j];
+                    if (numbers[i] < numbers[j])
                     {
                         int tempNub = numbers[i];
                         numbers[i] = numbers[j];
